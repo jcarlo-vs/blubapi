136630c29b7bd22118e176e8909edd79cb9d2adb
JC Senin <senin.juancarlo@gmail.com>
2024-08-25 17:30:35 +0800

    handle cors

--- api/cmc.js
+++ api/cmc.js
@@ -4,6 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 // api/hello.js
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allows requests from any origin
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
   try {
     const options = {
       headers: {
@@ -14,7 +23,7 @@ export default async function handler(req, res) {
       "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=24478,29150,29743";
 
     const { data } = await axios.get(api, options);
-    res.status(200).json({ message: data });
+    res.status(200).json({ data: data });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
